d0548707feb6a5bd839436ca89d2f89278f0f91d
Mario De Hoyos F <mariolodehf@gmail.com>
2025-08-25 14:48:55 -0500

    feat(blog): add RSS feed, tag pages scaffold, date/xml filters

--- .eleventy.blog.cjs
+++ .eleventy.blog.cjs
@@ -12,6 +12,21 @@ module.exports = function(eleventyConfig) {
     return Math.max(1, Math.round(words / 200));
   });
 
+  // Date RFC822 for RSS
+  eleventyConfig.addFilter('dateRFC822', dateObj => {
+    return (dateObj instanceof Date ? dateObj : new Date(dateObj)).toUTCString();
+  });
+
+  // Basic XML escape
+  eleventyConfig.addFilter('xmlEscape', str => {
+    if(!str) return '';
+    return String(str)
+      .replace(/&/g,'&amp;')
+      .replace(/</g,'&lt;')
+      .replace(/>/g,'&gt;')
+      .replace(/"/g,'&quot;');
+  });
+
   return {
     dir: {
       input: 'blog-src',
@@ -21,6 +36,6 @@ module.exports = function(eleventyConfig) {
     },
     templateFormats: ['md','njk','html'],
     markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk'
+  htmlTemplateEngine: 'njk'
   };
 };
